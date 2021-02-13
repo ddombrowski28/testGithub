096540530cb1a06e727321464d11a0c604e0aa30
awshort <59718017+awshort@users.noreply.github.com>
2021-02-13 10:47:48 -0800

    changed 3 to 6 in line 6!

--- test_project/test_module.py
+++ test_project/test_module.py
@@ -3,7 +3,7 @@ def add_squares(x, y):
     return x**2 + y**2
 
 def unit_test():
-    out1 = add_squares(3, 5)
+    out1 = add_squares(6, 5)
     out2 = add_squares(4, 4)
     out3 = add_squares(8, 0)
 
